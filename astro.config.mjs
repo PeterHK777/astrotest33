fa44a1409943741d5129b8a83969fa4d80617db6
PeterHK777 <peterhartmann@me.com>
2026-02-01 01:48:28 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://peterhk777.github.io",
+  base: "/astrotest33/",
   vite: {
     plugins: [tailwindcss()]
   }
